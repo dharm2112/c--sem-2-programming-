7c9e8ea95389e305d5228df5498e83820609ea26
dharm2112 <gabanidharmce24@gmail.com>
2025-04-08 22:31:00 +0530

    Update 1_1.cpp

--- 1_1.cpp
+++ 1_1.cpp
@@ -11,62 +11,59 @@ class BankAccount
 public:
     void getdata()
      {
-        cout << "\nEnter your name: ";
+        cout << "Enter your name: ";
         cin >> name;
-        cout << "\nEnter your account number: ";
+        cout << "Enter your account number: ";
         cin >> number;
-        cout << "\nEnter your bank balance: ";
+        cout << "Enter your bank balance: ";
         cin >> balance;
     }
-
     void deposit(int amount) {
         balance += amount;
     }
-
     void withdraw(int amount) {
         if (balance - amount >= 3000) {
             balance -= amount;
             cout << "\nCongratulations, you have withdrawn " << amount << ".";
         } else {
-            cout << "\nInsufficient balance. Minimum balance of 3000 must be maintained.";
+            cout << "\nInsufficient balance. ";
         }
     }
-
     void display_balance()
      {
-        cout << "\nBalance = " << balance << endl;
-        cout << "\n ------------------------\nthis program made by 24CE030 - DHARM .";
+        cout << "Balance = " << balance << endl;
+        cout << " ------------------------"<< endl;
     }
 };
 
 int main()
  {
-    int numAccounts;
+    int n;
     cout << "Enter the number of accounts to create: ";
-    cin >> numAccounts;
+    cin >> n;
     
-    BankAccount accounts[numAccounts];
+    BankAccount accounts[n];
     
-    for (int i=0; i<numAccounts;i++)
+    for (int i=0; i<n;i++)
      {
-        cout << "\nEnter details for account " << i+1 << ":";
+        cout << "\nEnter details for account " << i+1 << ":"<< endl;
         accounts[i].getdata();
     }
     
-    for (int i=0; i<numAccounts;i++) 
+    for (int i=0; i<n;i++) 
     {
         int amount1, amount2;
-        cout << "\nAccount " << i+1 << " transactions:";
-        cout << "\nEnter amount to deposit: ";
+        cout << "\nAccount " << i+1 << " transactions:" << endl;
+        cout << "Enter amount to deposit: ";
         cin >> amount1;
         accounts[i].deposit(amount1);
         
-        cout << "\nEnter amount to withdraw: ";
+        cout << "Enter amount to withdraw: ";
         cin >> amount2;
         accounts[i].withdraw(amount2);
         
         accounts[i].display_balance();
-    }
-    
+    }  
+    cout << "this program made by 24CE030 - DHARM .";
     return 0;
 }
