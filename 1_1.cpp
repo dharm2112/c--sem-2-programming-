fc376c4e15d6f18b47617a0c3c0a72193a33f349
dharm2112 <gabanidharmce24@gmail.com>
2025-04-09 17:39:45 +0530

    Update 1_1.cpp

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,4 +1,8 @@
 //This program made by 24CE030.
+/*1.1 Bank Account Management System
+This system manages customer accounts by allowing deposits, withdrawals (with balance checks), and balance inquiries.
+ It securely stores the account holder’s name, unique account number, and balance. 
+ Object-oriented principles like encapsulation and methods are used for secure access.*/
 #include <iostream>
 using namespace std;
 
@@ -7,11 +11,10 @@ class BankAccount
     char name[15];
     int number;
     float balance;
-
 public:
     void getdata()
      {
-        cout << "Enter your name: ";
+        cout << "\nEnter your name: ";
         cin >> name;
         cout << "Enter your account number: ";
         cin >> number;
@@ -26,13 +29,13 @@ public:
             balance -= amount;
             cout << "\nCongratulations, you have withdrawn " << amount << ".";
         } else {
-            cout << "\nInsufficient balance. ";
+            cout << "\nInsufficient balance. .";
         }
     }
     void display_balance()
      {
         cout << "Balance = " << balance << endl;
-        cout << " ------------------------"<< endl;
+        cout << "\n ------------------------"<< endl;
     }
 };
 
@@ -46,15 +49,15 @@ int main()
     
     for (int i=0; i<n;i++)
      {
-        cout << "\nEnter details for account " << i+1 << ":"<< endl;
+        cout << "\nEnter details for account " << i+1 << ":";
         accounts[i].getdata();
     }
     
     for (int i=0; i<n;i++) 
     {
         int amount1, amount2;
-        cout << "\nAccount " << i+1 << " transactions:" << endl;
-        cout << "Enter amount to deposit: ";
+        cout << "\nAccount " << i+1 << " transactions:";
+        cout << "\nEnter amount to deposit: ";
         cin >> amount1;
         accounts[i].deposit(amount1);
         
@@ -63,7 +66,7 @@ int main()
         accounts[i].withdraw(amount2);
         
         accounts[i].display_balance();
-    }  
-    cout << "this program made by 24CE030 - DHARM .";
+    }
+    cout << "this program made by 24CE030 - DHARM ." << endl;
     return 0;
 }
